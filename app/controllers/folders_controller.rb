862e21b8b1c177502092a3b5cbb1edef7c7bf62e
shunsuke-oka <71703108+shunsuke-oka@users.noreply.github.com>
2020-11-09 08:15:50 +0900

    フォルダ作成機能の実装

--- app/controllers/folders_controller.rb
+++ app/controllers/folders_controller.rb
@@ -1,4 +1,20 @@
 class FoldersController < ApplicationController
   def index
+    @folder = Folder.new
+  end
+
+  def create
+    @folder = Folder.new(folder_params)
+    if @folder.save
+      redirect_to root_path
+    else
+      render :index
+    end
+  end
+
+  private
+
+  def folder_params
+    params.require(:folder).permit(:title).merge(user_id: current_user.id)
   end
 end
---
+++ app/javascript/folder.js
@@ -0,0 +1,19 @@
+function memo() {
+  const createFolder = document.getElementById("create-folder");
+  const form = document.getElementById("form");
+
+  createFolder.addEventListener("mouseover", () => {
+    createFolder.setAttribute("style", "background-color:#37373d;")
+  })
+  createFolder.addEventListener("mouseout", () => {
+    createFolder.removeAttribute("style", "background-color:#000000;")
+  })
+  createFolder.addEventListener("click", () => {
+    if (form.getAttribute("style") == "display:block;") {
+      form.removeAttribute("style", "display:block;")
+    } else {
+      form.setAttribute("style", "display:block;")
+    }
+  })
+}
+setInterval(memo, 1000);
